86c1febff696e12aa90c7832454b8294abae3f47
jane arc <jane.arc@pobox.com>
2024-09-27 19:37:30 -0700

    k this looks reasonable
    next:
    * update makefile with make local target

--- main.go
+++ main.go
@@ -1,10 +1,28 @@
 package main
 
 import (
+	simps "bs/simple"
+	"flag"
 	"github.com/sirupsen/logrus"
+	"os"
 )
 
 func main() {
-	logrus.Info("well i guess that worked")
+	simple := flag.Bool("simple", false, "simple interaction")
+	query := flag.String("query", "", "query to pass to the backend")
+	flag.Parse()
 
+	if *simple {
+		logrus.Info("simple mode")
+
+		// simple mode
+		simp := simps.NewSimpleService(os.Getenv("SIMPLE_ROOT"))
+
+		rsp := simp.Chat(*query)
+		if rsp == "" {
+			logrus.Fatalf("Whoops, looks like a little too simple")
+		}
+
+		logrus.Infof("Response: %s", rsp)
+	}
 }
